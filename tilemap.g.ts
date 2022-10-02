e1edf956045d724adfddb5111c2e358513564764
TripleOxygen <99085361+TripleOxygen345@users.noreply.github.com>
2022-10-02 10:09:49 +0300

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,100 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+            case "forsty times":
+            case "forsty times1":return tiles.createTilemap(hex`320032000e0704040404040404040404040404040404040404040404040404040404040404040404040404040404040404040404040a0e050e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0704040404040404040404040404040404040404040404040404060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e02010101010101010101010101010101010101010101010101010f0e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e02010101010101010101010101010101010101010101010101010f0e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0201010101070d0d0d0d0d0d0d0d0d0d0d0d0d0d0d0d0d0d0d0d060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0201010101060e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0201010101060e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0201010101060e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0201010101060e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0201010101060e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0201010101060e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0201010101060e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0201010101060e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0704040404040404041001010101060e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0501010101010101010101010101060e0e0e0e0e0e0e070404040a0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0501010101010101010101010101060e0e0e0e0e0e0e020101010b0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0501010101010101010101010101060e0e0e0e0e0e0e02010c010b0e0e0e0e0e0e0e0e060e020e0e0e0e0e0e0e0e0e0e0e0e0501010101010101010101010101060e0e0e0e0e0e0e020101010b0e0e0e0e0e0e0e0e060e020404040404040404040404040401010404040404040404040404040404040404040404040104040404040404040404060e020101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101060e020101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101060e050101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101060e08030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030309`, img`
+.2222222222222222222222222222222222222222222222222
+.2...............................................2
+.2...............................................2
+.2...............................................2
+.2...............................................2
+.2...............................................2
+.2...............................................2
+.2...............................................2
+.2...............................................2
+.2...............................................2
+.2...............................................2
+.2...............................................2
+.2...............................................2
+.2...............................................2
+.2...............................................2
+.2...............................................2
+.2...............................................2
+.2...............................................2
+.2...............................................2
+.2...............................................2
+.2...............................................2
+.2...............................................2
+.2...............................................2
+.2...............................................2
+.2...............................................2
+.2...............................................2
+.2...............................................2
+.2...............................................2
+.2.....................222222222222222222222222222
+.2.....................2..........................
+.2.....................2..........................
+.2.....................2....2222222222222222222222
+.2.....................2....2....................2
+.2.....................2....2....................2
+.2.....................2....2....................2
+.2.....................2....2....................2
+.2.....................2....2....................2
+.2.....................2....2....................2
+.2.....................2....2....................2
+.2.....................2....2....................2
+.2............2222222222....2....................2
+.2............2.............2.......22222........2
+.2............2.............2.......2...2........2
+.2............2.............2.......2...2........2
+.2............2.............2.......2...2........2
+.22222222222222..222222222222222222222.22222222222
+.2...............................................2
+.2...............................................2
+.2...............................................2
+.2222222222222222222222222222222222222222222222222
+`, [myTiles.transparency16,sprites.dungeon.floorLight2,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterSouth1,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast1,sprites.dungeon.floorLight5,sprites.dungeon.greenOuterSouth0,myTiles.tile2,sprites.dungeon.doorOpenEast,sprites.dungeon.greenInnerSouthEast], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
