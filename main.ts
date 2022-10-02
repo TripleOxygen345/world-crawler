dfe6ac75bfecc93742e29aacdebaf829d193d9c5
TripleOxygen <99085361+TripleOxygen345@users.noreply.github.com>
2022-10-02 11:50:19 +0300

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.doorOpenEast, function (sprite, location) {
     color.FadeToBlack.startScreenEffect(1000)
-    tiles.setCurrentTilemap(tilemap` `)
+    collectedcrystals = 0
+    tiles.setCurrentTilemap(tilemap`under world`)
+    tiles.placeOnTile(jan, tiles.getTileLocation(25, 47))
+    color.startFadeFromCurrent(color.originalPalette, 2000)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.floorLight5, function (sprite, location) {
     pause(1000)
@@ -8,13 +11,18 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.floorLight5, function (sp
     music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 3498, 1, 255, 0, 216, SoundExpressionEffect.Warble, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
     game.over(false)
 })
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleInsignia, function (sprite, location) {
+    collectedcrystals += 1
+})
+let collectedcrystals = 0
+let jan: Sprite = null
 scene.setBackgroundImage(assets.image`myImage`)
 pause(1000)
 music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 912, 717, 255, 0, 3000, SoundExpressionEffect.None, InterpolationCurve.Logarithmic), SoundExpressionPlayMode.InBackground)
 color.FadeToBlack.startScreenEffect()
 pause(1000)
 tiles.setCurrentTilemap(tilemap`forsty times`)
-let jan = sprites.create(assets.image`JAn`, SpriteKind.Player)
+jan = sprites.create(assets.image`JAn`, SpriteKind.Player)
 scene.cameraFollowSprite(jan)
 tiles.placeOnTile(jan, tiles.getTileLocation(26, 48))
 controller.moveSprite(jan, 100, 100)
